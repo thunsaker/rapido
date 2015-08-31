899c3245d6a9478eda41e16fbc0620151cdc0d81
Thomas Hunsaker <thomas.hunsaker@gmail.com>
2015-08-31 14:06:35 -0700

    Moved most of my secrets to env vars, yay security!

--- mobile/src/main/java/com/thunsaker/rapido/services/AuthHelper.java
+++ mobile/src/main/java/com/thunsaker/rapido/services/AuthHelper.java
@@ -1,16 +1,18 @@
 package com.thunsaker.rapido.services;
 
+import com.thunsaker.rapido.BuildConfig;
+
 public class AuthHelper {
     // Twitter
-    public final static String TWITTER_KEY = System.getenv("RAPIDO_TWIT_KEY");
-    public final static String TWITTER_SECRET = System.getenv("RAPIDO_TWIT_SECRET");
+    public final static String TWITTER_KEY = BuildConfig.TWITTER_KEY;
+    public final static String TWITTER_SECRET = BuildConfig.TWITTER_SECRET;
 
     // Bit.ly
-    public final static String BITLY_CLIENT_ID = System.getenv("RAPIDO_BIT_ID");
-    public final static String BITLY_CLIENT_SECRET = System.getenv("RAPIDO_BIT_SECRET");
+    public final static String BITLY_CLIENT_ID = BuildConfig.BITLY_ID;
+    public final static String BITLY_CLIENT_SECRET = BuildConfig.BITLY_SECRET;
     public final static String BITLY_REDIRECT_URL = "http://thomashunsaker.com/apps/rapido";
 
     // Foursquare
-    public final static String FOURSQUARE_CLIENT_ID = System.getenv("RAPIDO_FOUR_ID");
-    public final static String FOURSQUARE_CLIENT_SECRET = System.getenv("RAPIDO_FOUR_SECRET");
+    public final static String FOURSQUARE_CLIENT_ID = BuildConfig.FOURSQUARE_ID;
+    public final static String FOURSQUARE_CLIENT_SECRET = BuildConfig.FOURSQUARE_SECRET;
 }
